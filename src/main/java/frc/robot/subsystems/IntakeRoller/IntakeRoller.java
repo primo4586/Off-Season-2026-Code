66a80647d218e9e337fee72481e65f990a14b8d6
Ariel <arielmevorachlevi@gmail.com>
2026-08-27 18:33:42 +0300

    added intake mechanisem and commands

--- src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRoller.java
+++ src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRoller.java
@@ -1,15 +1,28 @@
 package frc.robot.subsystems.IntakeRoller;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inch;
+import static edu.wpi.first.units.Units.RPM;
+
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.AngularVelocity;
+
 import static frc.robot.subsystems.IntakeRoller.IntakeRollerConstants.*;
+
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class IntakeRoller extends SubsystemBase {
      private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
@@ -18,10 +31,46 @@ public class IntakeRoller extends SubsystemBase {
   .withSimClosedLoopController(SIM_KP, SIM_KI, SIM_KD)
   .withFeedforward(new SimpleMotorFeedforward(REAL_KS, REAL_KV, REAL_KA))
   .withSimFeedforward(new SimpleMotorFeedforward(SIM_KS, SIM_KV, SIM_KA))
-  .withTelemetry("ShooterMotor", TELEMATRY_MODE)
+  .withTelemetry("IntakeMotor", MOTOR_TELEMATRY_MODE)
   .withGearing(GEARING)
   .withMotorInverted(INVERTED)
   .withIdleMode(MOTOR_MODE)
   .withStatorCurrentLimit(STATOR_CURRENT_LIMIT)
   .withStatorCurrentLimit(SUPPLY_CURRENT_LIMIT);
+
+
+  private TalonFX talonFX = new TalonFX(MOTOR_ID);
+  private SmartMotorController motor = new TalonFXWrapper(talonFX, DCMotor.getFalcon500(1), smcConfig);
+  private final FlyWheelConfig intakeRollerConfig = new FlyWheelConfig()
+  .withDiameter(Inch.of(2))
+  // Telemetry name and verbosity for the arm.
+  .withTelemetry("IntakeRollerMech", FLYWHEEL_TELEMATRY_MODE);
+   private FlyWheel intake = new FlyWheel(intakeRollerConfig, motor);
+   /**
+    * a command that runs the motor at desired angular velocity
+    * @param speed
+    * @return
+    */
+   public Command intake() {return intake.run(RPM.of(INTAKE_SPEED));}
+   public Command outake(){return intake.run(RPM.of(-INTAKE_SPEED));}
+   /**
+    * gives spped of motor
+    * @return AngularVelocity of motor
+    */
+    public AngularVelocity getVelocity(){return intake.getSpeed();}
+    
+    public Command set(double dutyCycle){return intake.set(dutyCycle);}
+
+
+    @Override
+    public void periodic(){
+        intake.updateTelemetry();
+    }
+    @Override
+    public void simulationPeriodic(){
+        intake.simIterate();
+    }
+
+
+ 
 }
--- src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRollerConstants.java
+++ src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRollerConstants.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.IntakeRoller;
 
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RPM;
 
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
@@ -9,7 +11,7 @@ import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 public class IntakeRollerConstants {
     public static final ControlMode CONTROL_MODE = ControlMode.CLOSED_LOOP;
     //Real PID constants
-    public static final float REAL_KP = 0;
+    public static final float REAL_KP = 50;
     public static final float REAL_KI = 0;
     public static final float REAL_KD = 0;
      //Real FEEDFORWARD constants
@@ -17,7 +19,7 @@ public class IntakeRollerConstants {
     public static final float REAL_KV = 0;
     public static final float REAL_KA = 0;
     //SIM PID constants
-    public static final float SIM_KP = 0;
+    public static final float SIM_KP = 50;
     public static final float SIM_KI = 0;
     public static final float SIM_KD = 0;
      //SIM FEEDFORWARD constants
@@ -28,9 +30,14 @@ public class IntakeRollerConstants {
     public static final double GEARING = 3;
     public static final boolean INVERTED = false;
     public static final MotorMode MOTOR_MODE= MotorMode.COAST;
+    public static final int MOTOR_ID = 2;
+    public static final int INTAKE_SPEED = 60;
     //LIMITS
     public static final Current STATOR_CURRENT_LIMIT = Amps.of(100);
     public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(50);
     //Telematry
-    public static final TelemetryVerbosity TELEMATRY_MODE = TelemetryVerbosity.HIGH;
+    public static final TelemetryVerbosity MOTOR_TELEMATRY_MODE = TelemetryVerbosity.HIGH;
+    public static final TelemetryVerbosity FLYWHEEL_TELEMATRY_MODE = TelemetryVerbosity.HIGH;
+    
+    
 }
