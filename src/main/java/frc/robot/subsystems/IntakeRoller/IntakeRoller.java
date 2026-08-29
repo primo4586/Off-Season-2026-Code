0d523b42f5af4fdad99ae4e0e03ceb58729e9111
Ariel <arielmevorachlevi@gmail.com>
2026-08-30 01:31:37 +0300

    after uri's comments

--- src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRoller.java
+++ src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRoller.java
@@ -2,23 +2,19 @@ package frc.robot.subsystems.IntakeRoller;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Inch;
 import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Volts;
 
-import yams.gearing.GearBox;
-import yams.gearing.MechanismGearing;
 import yams.mechanisms.config.FlyWheelConfig;
 import yams.mechanisms.velocity.FlyWheel;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
-import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
 
 import static frc.robot.subsystems.IntakeRoller.IntakeRollerConstants.*;
 
@@ -46,19 +42,55 @@ public class IntakeRoller extends SubsystemBase {
   // Telemetry name and verbosity for the arm.
   .withTelemetry("IntakeRollerMech", FLYWHEEL_TELEMATRY_MODE);
    private FlyWheel intake = new FlyWheel(intakeRollerConfig, motor);
+
+
    /**
-    * a command that runs the motor at desired angular velocity
-    * @param speed
+    * a command that runs intakes with a preSelected speed
     * @return
     */
    public Command intake() {return intake.run(RPM.of(INTAKE_SPEED));}
-   public Command outake(){return intake.run(RPM.of(-INTAKE_SPEED));}
    /**
-    * gives spped of motor
+    * a command that runs outakes with a preSelected speed
+    * @return
+    */
+   public Command outake(){return intake.run(RPM.of(-INTAKE_SPEED));}
+
+     /**
+    * a command that runs intakes with a preSelected voltage
+    * @return
+    */
+   public Command intakeWithVoltage(){return intake.setVoltage(Volts.of(INTAKE_VOLTAGE));}
+   
+     /**
+    * a command that runs outakes with a preSelected voltage
+    * @return
+    */
+   public Command outakeWithVoltage(){return intake.setVoltage(Volts.of(-INTAKE_VOLTAGE));}
+ /**
+    * a command that runs intakes with a Selected speed
+    * @param intakeVelocity - desired AngularVelocity
+    * @return
+    */
+   public Command intake(AngularVelocity intakeVelocity) {return intake.run(intakeVelocity);}
+   /**
+    * a command that runs outakes with a Selected voltage
+    * @param intakeVoltage - desired voltage
+    * @return
+    */
+   public Command intakeWithVoltage(Voltage intakeVoltage){return intake.setVoltage(intakeVoltage);}
+   
+    
+   /**
+    * gives current spped of motor
     * @return AngularVelocity of motor
     */
     public AngularVelocity getVelocity(){return intake.getSpeed();}
     
+     /**
+    * a command that runs the motor at desired precent of its maximum power until interrupted
+    * @param dutyCylce - a number in [-1,1] that determines the precnet of the maximum power that the motor will run at  
+    * @return 
+    */
     public Command set(double dutyCycle){return intake.set(dutyCycle);}
 
 
--- src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRollerConstants.java
+++ src/main/java/frc/robot/subsystems/IntakeRoller/IntakeRollerConstants.java
@@ -1,40 +1,39 @@
 package frc.robot.subsystems.IntakeRoller;
 
 import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.RPM;
 
-import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 public class IntakeRollerConstants {
+    public static final int MOTOR_ID = 2;
     public static final ControlMode CONTROL_MODE = ControlMode.CLOSED_LOOP;
     //Real PID constants
-    public static final float REAL_KP = 50;
-    public static final float REAL_KI = 0;
-    public static final float REAL_KD = 0;
+    public static final double REAL_KP = 50;
+    public static final double REAL_KI = 0;
+    public static final double REAL_KD = 0;
      //Real FEEDFORWARD constants
-    public static final float REAL_KS = 0;
-    public static final float REAL_KV = 0;
-    public static final float REAL_KA = 0;
+    public static final double REAL_KS = 0;
+    public static final double REAL_KV = 0;
+    public static final double REAL_KA = 0;
     //SIM PID constants
-    public static final float SIM_KP = 50;
-    public static final float SIM_KI = 0;
-    public static final float SIM_KD = 0;
+    public static final double SIM_KP = 50;
+    public static final double SIM_KI = 0;
+    public static final double SIM_KD = 0;
      //SIM FEEDFORWARD constants
-    public static final float SIM_KS = 0;
-    public static final float SIM_KV = 0;
-    public static final float SIM_KA = 0;
+    public static final double SIM_KS = 0;
+    public static final double SIM_KV = 0;
+    public static final double SIM_KA = 0;
     //Physical Constants
     public static final double GEARING = 3;
     public static final boolean INVERTED = false;
     public static final MotorMode MOTOR_MODE= MotorMode.COAST;
-    public static final int MOTOR_ID = 2;
-    public static final int INTAKE_SPEED = 60;
+    public static final double INTAKE_SPEED = 60;
+    public static final double INTAKE_VOLTAGE = 10;
     //LIMITS
-    public static final Current STATOR_CURRENT_LIMIT = Amps.of(100);
-    public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(50);
+    public static final Current STATOR_CURRENT_LIMIT = Amps.of(120);
+    public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(60);
     //Telematry
     public static final TelemetryVerbosity MOTOR_TELEMATRY_MODE = TelemetryVerbosity.HIGH;
     public static final TelemetryVerbosity FLYWHEEL_TELEMATRY_MODE = TelemetryVerbosity.HIGH;
